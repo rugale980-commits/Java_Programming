5544d08f84771a52b127567ee4052c7c02ac9910
Rahul Ugale <rugale980@gmail.com>
2025-11-23 14:53:28 +0530

    Add files via upload

---
+++ Assignments/Assignment_31/program31_3.java
@@ -0,0 +1,88 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Display
+//  Description   : Accept N number from user & display pattren.
+//  Input         : Charcter
+//  Output        : Integer
+//  Author        : Rahul Balasaheb Ugale
+//  Date          : 22/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////
+
+import java.util.*;
+
+public class Pattern
+{
+    public void Display(int iRow,int iCol)
+    {
+        int iCnt = 0;
+        int jCnt = 0;
+
+        if (iRow != iCol)
+        {
+            System.out.print("Invalid Input");
+            System.out.print("Row number and column number should be same");
+            return;
+        }
+        for (iCnt = 1; iCnt <= iRow; iCnt++)
+        {
+            for (jCnt = 1; jCnt <= iCol; jCnt++)
+            {
+                if ((iCnt == jCnt))
+                {
+                    System.out.print("%\t");
+                }
+                else if (iCnt < jCnt)
+                {
+                    System.out.print("#\t");
+                }
+                else
+                {
+                    System.out.print("*\t");
+                }
+            }
+            System.out.print("\n");
+        }
+} // End of function
+}
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+////////////////////////////////////////////////////////////////////////////////
+
+public class program31_3
+{
+    public static void main(String A[])
+    {
+        int iValue1 = 0;
+        int iValue2 = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter number of Rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter number of Columns : ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1,iValue2);
+    }
+
+} // End of main
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Testcases succesfully handle by the application
+//
+//  Input  : iRow = 5 , iCol = 5 :
+//
+//  Output : %       #       #       #       #
+//           *       %       #       #       #
+//           *       *       %       #       #
+//           *       *       *       %       #
+//           *       *       *       *       %
+//
+////////////////////////////////////////////////////////////////////////////////
